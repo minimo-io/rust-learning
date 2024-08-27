01b97589ec082ac7274f8e2f6ebe2c96ac65978d
minimo-io <nicolas@minimo.io>
2024-08-26 23:30:09 -0300

    data types

---
+++ 2-data-types/src/main.rs
@@ -0,0 +1,6 @@
+// Rust is statically typed meaning that it must know the types of all variables at compile time.
+// Scalar types represent a single value: char, boolean, integer, floating-point number.
+
+fn main() {
+    println!("Hello, world!");
+}
