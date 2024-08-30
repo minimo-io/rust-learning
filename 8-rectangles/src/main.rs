e29e43d9173262ab933a5a7d24bffafa9ec25d9d
minimo-io <nicolas@minimo.io>
2024-08-30 17:52:05 -0300

    add debugging stuff

--- 8-rectangles/src/main.rs
+++ 8-rectangles/src/main.rs
@@ -1,13 +1,18 @@
+#[derive(Debug)]
 struct Rectangle{
     width: u32,
     height:u32
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle{
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50
     };
+    println!("rect 1 is {rect1:#?}"); // :? tells println! that we want to use an output called Debug
+    // dbg macro outputs to the stderr instead of the stadout
+    dbg!(&rect1);
 
     println!(
         "The area of the rectangle is {} square pixels.",
