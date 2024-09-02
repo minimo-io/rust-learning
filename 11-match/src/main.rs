c87eb707ac807771f974838a9d9ef8e152eb1ba9
minimo-io <nicolas@minimo.io>
2024-09-01 21:40:03 -0300

    match and if let

--- 11-match/src/main.rs
+++ 11-match/src/main.rs
@@ -33,9 +33,53 @@ fn plus_one(x: Option<i32>) ->Option<i32>{
         Some(i) => Some(i + 1)
     }
 }
+// But hey, using match we can also have catch-all patters
+// the catch all arm must be last
+// Here is an example of a catch call were we won't use the value
+// so we use the special _ catch all:
+
+// let dice_roll = 9;
+fn lets_roll(dice_roll: u8){
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        _ => (), // unit value; nothing happends if our dice roll it is not 3 or 7 (in our game)
+    }
+}
+fn add_fancy_hat(){}
+fn remove_fancy_hat(){}
+fn reroll(){}
+
+
 fn main() {
     let five = Some(5);
     let six = plus_one(five);
     let none = plus_one(None);
     dbg!(six);
-}
+
+    // the "if let " control flow is the same a match for cases were we ca
+    // only for one match and want to ignore the rest, 
+    // _ => ()
+    let config_max = Some(3u8);
+    if let Some(max) = config_max{
+        println!("The maximum is configured to be {max}");
+    }    
+    // is the same as:
+    // let config_max = Some(3u8);
+    // match config_max {
+    //     Some(max) => println!("The maximum is configured to be {max}"),
+    //     _ => (),
+    // }
+    // the code in the if let does not run, if the value (config_max)
+    // doesn't match the pattern Some(max)
+
+    // we can also use and else in the "if let" flow control 
+    // that would be the same as the case _ => { //something } in a match 
+
+    if let Some(max) = config_max{
+        // something
+    }else{
+        // same code we would place in a _ => {} match
+    }
+}   
+    
