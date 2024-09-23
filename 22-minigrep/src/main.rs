e560358b5d57884fb41b1f282669b33a4fc56d63
minimo-io <nicolas@minimo.io>
2024-09-23 16:26:21 -0300

    mini grep progress!

--- 22-minigrep/src/main.rs
+++ 22-minigrep/src/main.rs
@@ -3,15 +3,31 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let config: Config = Config::new(&args);
+    // dbg!(args);
 
-    println!("In file {file_path}");
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file.");
+    println!("In file {}", config.query);
+    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file.");
 
     println!("Wiht text: \n{contents}");
 
     // println!("Searching {query}");
 
 
+}
+struct Config{
+    query: String,
+    file_path: String
+}
+impl Config{
+    fn new(args: &[String]) -> Config{
+        if args.len() < 3{
+            panic!("not enough arguments");
+        }
+
+        let query:String = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Config{query, file_path}
+    }
 }
