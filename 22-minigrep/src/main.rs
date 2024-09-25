4230d0b2bbbcaf75e5cfec47731230c49313b4d0
minimo-io <nicolas@minimo.io>
2024-09-24 22:18:28 -0300

    closures and interators

--- 22-minigrep/src/main.rs
+++ 22-minigrep/src/main.rs
@@ -9,6 +9,7 @@ fn main() {
     // these are anonymous functions passed to the unwrap_or_else
     // to handle the error case of our Result
     let config: Config = Config::build(&args).unwrap_or_else(|err|{
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
@@ -16,6 +17,7 @@ fn main() {
     // Instead of using a unwrap_or_else, in this case we do not have a value to unwrap (run fn retuns a
     // unit vale instead), so we only care of handling the Error case, that's why we use an "if let"
     if let Err(e) = minigrep::run(config){
+        eprintln!("Application error {e}");
         process::exit(1);
     }
 
