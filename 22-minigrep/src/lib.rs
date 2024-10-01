0bc1218a8ed2f8cbf71e19e4c2da07873d2328f6
minimo-io <nicolas@minimo.io>
2024-09-30 22:49:04 -0300

    using interators as function params

--- 22-minigrep/src/lib.rs
+++ 22-minigrep/src/lib.rs
@@ -1,78 +1,90 @@
-use std::{fs, vec};
-use std::error::Error;
 use std::env;
+use std::error::Error;
+use std::{fs, vec};
 
-pub struct Config{
+pub struct Config {
     pub query: String,
     pub file_path: String,
     pub ignore_case: bool,
 }
-impl Config{
-   pub fn build(args: &[String]) -> Result<Config, &'static str>{
-        if args.len() < 3{
-            return Err("not enough arguments")
-        }
+impl Config {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
 
-        let query:String = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        // if args.len() < 3 {
+        //     return Err("not enough arguments");
+        // }
+
+        // let query: String = args[1].clone();
+        // let file_path = args[2].clone();
+        //
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         // is_ok() returns true if the Result is Ok()
         let ignore_case = env::var("IGNORE_CASE").is_ok();
-    
-        Ok(Config{
-            query, 
-            file_path, 
-            ignore_case
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
         })
     }
 }
 
-// Box<dyn Error> means the function will return a type that implements the Error trait 
-// But we don’t have to specify what particular type the return value will be. 
-// Giving us the flexibility to return error values that may be of different types in different error cases. 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+// Box<dyn Error> means the function will return a type that implements the Error trait
+// But we don’t have to specify what particular type the return value will be.
+// Giving us the flexibility to return error values that may be of different types in different error cases.
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
     let results = if config.ignore_case {
         search_case_insensitive(&config.query, &contents)
-    }else{
+    } else {
         search(&config.query, &contents)
     };
 
-    for line in results{
+    for line in results {
         println!("{line}");
     }
     Ok(())
 }
 
 // Because contents is the argument that contains all of our text and we want to return the parts of that text that match, we know contents is the argument that should be connected to the return value using the lifetime syntax.
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
-    for line in contents.lines(){
-        if line.contains(query){
+    for line in contents.lines() {
+        if line.contains(query) {
             results.push(line);
         }
     }
     results
 }
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
 
-    for line in contents.lines(){
-        if line.to_lowercase().contains(&query){
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
             results.push(line);
         }
     }
     results
 }
-#[cfg(test)]    
-mod tests{
-    
+#[cfg(test)]
+mod tests {
+
     use super::*;
 
     #[test]
-    fn case_sensitive(){
+    fn case_sensitive() {
         let query = "duct";
         // this \ tells Rust to not include a new line character after it
         let contents = "\
@@ -84,7 +96,7 @@ Duct tape.";
     }
 
     #[test]
-    fn case_insensitive(){
+    fn case_insensitive() {
         let query = "rUsT";
         let contents = "\
 Rust:
@@ -94,8 +106,7 @@ Trust me.
         ";
         assert_eq!(
             vec!["Rust:", "Trust me."],
-            search_case_insensitive(query,contents)
+            search_case_insensitive(query, contents)
         );
     }
-
 }
--- 22-minigrep/src/main.rs
+++ 22-minigrep/src/main.rs
@@ -4,23 +4,19 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
     // Here with innovate with a closure for the first time in this tutorial
     // these are anonymous functions passed to the unwrap_or_else
     // to handle the error case of our Result
-    let config: Config = Config::build(&args).unwrap_or_else(|err|{
+    let config: Config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-
     // Instead of using a unwrap_or_else, in this case we do not have a value to unwrap (run fn retuns a
     // unit vale instead), so we only care of handling the Error case, that's why we use an "if let"
-    if let Err(e) = minigrep::run(config){
+    if let Err(e) = minigrep::run(config) {
         eprintln!("Application error {e}");
         process::exit(1);
     }
-
-
 }
-
